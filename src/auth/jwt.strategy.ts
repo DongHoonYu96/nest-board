6b6c8c357b2a682324954e8c252843399ecf07b6
DongHoonYu96 <sbna154@gmail.com>
2024-08-19 01:51:21 +0900

    refactor : 환경변수 설정으로 하드코딩 대체

--- src/auth/jwt.strategy.ts
+++ src/auth/jwt.strategy.ts
@@ -4,6 +4,8 @@ import { ExtractJwt, Strategy } from "passport-jwt";
 import { InjectRepository } from "@nestjs/typeorm";
 import { UserRepository } from "./user.repository";
 import { User } from "./user.entity"; //passport아님에 주의
+import * as config from 'config';
+
 
 @Injectable() //어디에서나 사용가능토록
 export class JwtStrategy extends PassportStrategy(Strategy){
@@ -12,7 +14,7 @@ export class JwtStrategy extends PassportStrategy(Strategy){
     private userRepository : UserRepository
   ) {
     super({
-      secretOrKey: 'Secret1234', //검증용 시크릿키, 모듈에서 설정해준거랑 같은값
+      secretOrKey: process.env.JWT_SECRET || config.get('jwt').secret, //검증용 시크릿키, 모듈에서 설정해준거랑 같은값
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken() //Bearer토큰 타입으로 올거임
     })
   }
--- src/configs/typeorm.config.ts
+++ src/configs/typeorm.config.ts
@@ -1,12 +1,15 @@
 import { TypeOrmModuleOptions } from "@nestjs/typeorm";
+import * as config from 'config';
+
+const dbConfig = config.get('db');
 
 export const typeORMConfig : TypeOrmModuleOptions = {
-  type: 'postgres',
-  host: 'localhost',
-  port: 5432,
-  username: 'postgres',
-  password: '123456',
-  database: 'board-app',
+  type: dbConfig.type,
+  host: process.env.RDS_HOSTNAME || dbConfig.host, //AWS or local 에서 모두 작동하도록
+  port: process.env.RDS_PORT || dbConfig.port,
+  username: process.env.RDS_USERNAME || dbConfig.username,
+  password: process.env.RDS_PASSWORD || dbConfig.password,
+  database: process.env.RDS_DB_NAME || dbConfig.database,
   entities: [__dirname + '/../**/*.entity.{js,ts}'], //엔티티 파일이 어디있는지(엔티티 이용해서 db table 생성함)
-  synchronize: true //true -> app 재실행시, 수정된 컬럼의 테이블 drop후 재생성
+  synchronize: dbConfig.synchronize //true -> app 재실행시, 수정된 컬럼의 테이블 drop후 재생성
 }
